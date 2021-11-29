5160e3f640369ce1afe906acad050da69615c769
ArsenyKokotov <52434320+ArsenyKokotov@users.noreply.github.com>
2021-11-29 01:12:34 -0500

    modded models

--- tmt_mood_translator/models.py
+++ tmt_mood_translator/models.py
@@ -3,15 +3,16 @@ from django.db import models
 class Input(models.Model):
     input=models.CharField(max_length=500)
     translation=models.CharField(max_length=1000, blank=True)
-    positivity=models.CharField(max_length=500)
+    evaluation=models.CharField(max_length=500)
 
 class Satisfaction(models.Model):
     satisfied=models.BooleanField()
 
 class User_Evaluation(models.Model):
-    angry=models.BooleanField()
-    sad=models.BooleanField()
     joy=models.BooleanField()
+    sadness=models.BooleanField()
     fear=models.BooleanField()
-    disgust=models.BooleanField()
+    anger=models.BooleanField()
+    neutral=models.BooleanField()
+
 
