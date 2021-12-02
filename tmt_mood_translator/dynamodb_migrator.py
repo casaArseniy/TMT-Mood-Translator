b8b6f5ca7546f39d2cee14b249c778e23bfbc8ae
ArsenyKokotov <52434320+ArsenyKokotov@users.noreply.github.com>
2021-12-01 22:12:20 -0500

    Update dynamodb_migrator.py

--- tmt_mood_translator/dynamodb_migrator.py
+++ tmt_mood_translator/dynamodb_migrator.py
@@ -9,7 +9,7 @@ import string
 def get_all():
     session = boto3.Session(
     aws_access_key_id='#',
-    aws_secret_access_key='#',
+    aws_secret_access_key='/',
     region_name='ca-central-1'
     )
 
@@ -25,11 +25,51 @@ def get_all():
     
     return data
 
+def get_all_feedback():
+    session = boto3.Session(
+        aws_access_key_id='#',
+        aws_secret_access_key='/',
+        region_name='ca-central-1'
+    )
+
+    DB=session.resource('dynamodb')
+    table=DB.Table("My_FeedBack_Table")
+    
+    response = table.scan()
+    data = response['Items']
+
+    while 'LastEvaluatedKey' in response:
+        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+        data.extend(response['Items'])
+    
+    return data
+
+def get_this_feedback(input):
+    session = boto3.Session(
+        aws_access_key_id='#',
+        aws_secret_access_key='#',
+        region_name='ca-central-1'
+    )
+
+    DB=session.resource('dynamodb')
+    table=DB.Table("My_FeedBack_Table")
+    
+    response = table.get_item(
+            Key={
+                'input': input
+            }
+        )
+
+    if 'Item' in response:
+        return True, response["Item"]
+    
+    return False, "NULL"
+
 
 def check_data(input):
     session = boto3.Session(
     aws_access_key_id='#',
-    aws_secret_access_key='#',
+    aws_secret_access_key='/',
     region_name='ca-central-1'
     )
 
@@ -71,7 +111,7 @@ def push_data(input, translation, evaluation):
 def randStr(chars=string.ascii_uppercase + string.digits, N=10):
     return ''.join(random.choice(chars) for _ in range(N))
 
-def push_feedback(input, angry, sad, joy, fear, disgust):
+def push_feedback(input, joy, sadness, fear, anger,  neutral):
 
     session = boto3.Session(
         aws_access_key_id='#',
