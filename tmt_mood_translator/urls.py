3bf9078327752e646e3249f7a2b24b2062102955
ArsenyKokotov <52434320+ArsenyKokotov@users.noreply.github.com>
2021-12-01 22:13:34 -0500

    Update urls.py

--- tmt_mood_translator/urls.py
+++ tmt_mood_translator/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('', views.home, name='home-page'),
     path('evaluation/<str:output>', views.evaluation_view, name='evaluation'),
     path('feedback/<str:output>', views.feedback, name='evaluation'),
-    path('API/', views.myAPI.as_view(), name='API-service'),
-    path('API/<str:input>/', views.myAPISingle.as_view(), name='API-service'),
+    path('API/analysis/', views.all_API_analysis.as_view(), name='API-service'),
+    path('API/feedback/', views.all_API_feedback.as_view(), name='API-service'),
+    path('API/analysis/<str:my_input>', views.myAPI_analysis.as_view(), name='API-service'),
+    path('API/feedback/<str:my_input>/', views.myAPIS_feedback.as_view(), name='API-service'),
 ]
